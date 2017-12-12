5052347b8584197cebe5b39384c989d30f3800b7
Bobby Lux <robert.michael.lux@gmail.com>
2017-12-12 17:02:06 -0500

    feat: add currentprayer and preprayer components

---
+++ components/CurrentPrayer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const CurrentPrayer = ({ loadNextPrayer }) => (
+  <View>
+    <Text>Filler</Text>
+  </View>
+)
+
+export default CurrentPrayer
