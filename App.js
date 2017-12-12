4948068aedd5d90f08cabf14e2b23e8d6ffdd99a
Bobby Lux <robert.michael.lux@gmail.com>
2017-12-12 12:32:29 -0500

    feat: add font and image caching to app.js

--- App.js
+++ App.js
@@ -1,14 +1,52 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { AppLoading, Asset, Font } from 'expo'
+import { StyleSheet, Image } from 'react-native'
 import Swiper from 'react-native-swiper'
-import Home from './components/Home'
-import Settings from './components/Settings'
-import Accept from './components/Accept'
-import Submit from './components/Submit'
-import Manage from './components/Manage'
+import { Settings, Home, Accept, Submit, Manage } from './components'
+
+function cacheImages(images) {
+  return images.map(image => {
+    if (typeof image === 'string') {
+      return Image.prefetch(image)
+    } else {
+      return Asset.fromModule(image).downloadAsync()
+    }
+  })
+}
+
+function cacheFonts(fonts) {
+  return fonts.map(font => Font.loadAsync(font))
+}
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isReady: false,
+    }
+  }
+
+  async _loadAssetsAsync() {
+    const imageAssets = cacheImages([
+      'https://static.pexels.com/photos/414727/pexels-photo-414727.jpeg',
+      require('./assets/images/milky-way.jpg'),
+    ])
+    const fontAssets = cacheFonts([{
+      'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'
+    )}])
+    await Promise.all([...imageAssets, ...fontAssets])
+  }
+
   render() {
+    if (!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this._loadAssetsAsync}
+          onFinish={() => this.setState({ isReady: true })}
+          onError={console.warn}
+        />
+      )
+    }
     return (
       <Swiper
         style={styles.wrapper}
---
+++ components/index.js
@@ -0,0 +1,5 @@
+export { default as Settings } from './Settings.js'
+export { default as Home } from './Home.js'
+export { default as Accept } from './Accept.js'
+export { default as Submit } from './Submit.js'
+export { default as Manage } from './Manage.js'
