23ed8364ca3cb7f8d6d3b74ce74a58854e4c8ded
Bobby Lux <robert.michael.lux@gmail.com>
2017-12-27 18:13:21 -0600

    feat: pass handleNotification down to loginForm

--- components/Submit/SubmitVerify.js
+++ components/Submit/SubmitVerify.js
@@ -8,6 +8,7 @@ export default class SubmitVerify extends React.Component {
     if (!this.props.isLoggedIn) {
       return (
         <LoginForm
+          handleNotification={this.props.handleNotification}
           verifyStorageKey={this.props.verifyStorageKey}
         />
       )
