6a8282315973df1ccd987638358a31d60ac1d546
Bobby Lux <robert.michael.lux@gmail.com>
2018-01-10 12:29:08 -0500

    feat: change submitVerify to presentation component and consolidate styling

--- components/Submit/SubmitVerify.js
+++ components/Submit/SubmitVerify.js
@@ -4,26 +4,24 @@ import LoginForm from './LoginForm'
 import SubmitForm from './SubmitForm'
 import styles from '../StyleSheet'
 
-export default class SubmitVerify extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.backgroundImage}>
-          <Image
-            source={require('../../assets/images/nightSky.jpg')}
-            style={{flex: 1, resizeMode: 'cover', width: '100%'}}
-          />
-        </View>
-        { !this.props.isLoggedIn
-          ? <LoginForm
-              verifyStorageKey={this.props.verifyStorageKey}
-            />
-          : <SubmitForm
-              fetchUserPrayers={this.props.fetchUserPrayers}
-              userId={this.props.userId}
-            />
-        }
-      </View>
-    )
-  }
-}
+const SubmitVerify = ({ isLoggedIn, verifyStorageKey, fetchUserPrayers, userId }) => (
+  <View style={styles.invisiContainer}>
+    <View style={styles.backgroundImageFrame}>
+      <Image
+        source={require('../../assets/images/nightSky.jpg')}
+        style={styles.backgroundImage}
+      />
+    </View>
+    { !isLoggedIn
+      ? <LoginForm
+          verifyStorageKey={verifyStorageKey}
+        />
+      : <SubmitForm
+          fetchUserPrayers={fetchUserPrayers}
+          userId={userId}
+        />
+    }
+  </View>
+)
+
+export default SubmitVerify
