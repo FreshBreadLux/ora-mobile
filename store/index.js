d301285a00d51d69ccf5a48040bba465a668ca3f
Bobby Lux <robert.michael.lux@gmail.com>
2018-02-01 11:18:05 -0500

    feat: take initial stab at prayers.js in store

---
+++ store/index.js
@@ -0,0 +1,15 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import createLogger from 'redux-logger'
+import thunkMiddleware from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import prayers from './prayers'
+
+const reducer = combineReducers({ prayers })
+const middleware = composeWithDevTools(applyMiddleware(
+  thunkMiddleware,
+  createLogger({collapsed: true})
+))
+const store = createStore(reducer, middleware)
+
+export default store
+export * from './prayers'
---
+++ store/prayers.js
@@ -0,0 +1,43 @@
+import axios from 'axios'
+import ROOT_URL from '../config'
+
+/**
+ * ACTION TYPES
+ */
+const GET_PRAYERS = 'GET_PRAYERS'
+const REMOVE_PRAYERS = 'REMOVE_PRAYERS'
+
+/**
+ * INITIAL STATE
+ */
+const defaultPrayers = null
+
+/**
+ * ACTION CREATORS
+ */
+export const getPrayers = prayers => ({ type: GET_PRAYERS, prayers })
+export const removePrayers = () => ({ type: REMOVE_PRAYERS })
+
+/**
+ * THUNK CREATORS
+ */
+export const fetchUserPrayers = userId =>
+  dispatch =>
+    axios.get(`${ROOT_URL}/api/users/${userId}/prayers`)
+      .then(res =>
+        dispatch(getPrayers(res.data || defaultPrayers)))
+      .catch(err => console.log(err))
+
+/**
+ * REDUCER
+ */
+export default function(state = defaultPrayers, action) {
+  switch (action.type) {
+    case GET_PRAYERS:
+     return action.prayers
+    case REMOVE_PRAYERS:
+      return defaultPrayers
+    default:
+     return state
+  }
+}
