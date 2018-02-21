05d7968f733aec2d5008726cdf650fe26ce040ed
Bobby Lux <robert.michael.lux@gmail.com>
2018-02-21 11:03:42 -0500

    feat: 10 planes in AirPlanePresenter

--- components/containers/introContainers/IntroSwiperContainer.js
+++ components/containers/introContainers/IntroSwiperContainer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, SafeAreaView, Animated } from 'react-native'
+import { View, SafeAreaView, Animated as Ad } from 'react-native'
 import { LinearGradient } from 'expo'
 import Swiper from 'react-native-swiper'
 import WelcomeContainer from './WelcomeContainer'
@@ -12,11 +12,28 @@ export default class IntroSwiperContainer extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      planeIntro: new Animated.Value(0),
-      plane1Fade: new Animated.Value(0),
-      plane2Fade: new Animated.Value(0),
-      plane2YVal: new Animated.Value(0),
-      planeOutro: new Animated.Value(0),
+      planeIntro: new Ad.Value(0),
+      plane1Fade: new Ad.Value(0),
+      plane1YVal: new Ad.Value(-100),
+      plane2Fade: new Ad.Value(0),
+      plane2YVal: new Ad.Value(-100),
+      plane3Fade: new Ad.Value(0),
+      plane3YVal: new Ad.Value(-100),
+      plane4Fade: new Ad.Value(0),
+      plane4YVal: new Ad.Value(-100),
+      plane5Fade: new Ad.Value(0),
+      plane5YVal: new Ad.Value(-100),
+      plane6Fade: new Ad.Value(0),
+      plane6YVal: new Ad.Value(-100),
+      plane7Fade: new Ad.Value(0),
+      plane7YVal: new Ad.Value(-100),
+      plane8Fade: new Ad.Value(0),
+      plane8YVal: new Ad.Value(-100),
+      plane9Fade: new Ad.Value(0),
+      plane9YVal: new Ad.Value(-100),
+      plane10Fade: new Ad.Value(0),
+      plane10YVal: new Ad.Value(-100),
+      planeOutro: new Ad.Value(0),
       alarmVisible: false
     }
     this.handleSwipe = this.handleSwipe.bind(this)
@@ -35,14 +52,32 @@ export default class IntroSwiperContainer extends React.Component {
   }
 
   fadeInPlane() {
-    Animated.sequence([
-      Animated.timing(this.state.planeIntro, { toValue: 1, duration: 1000 }),
-      Animated.parallel([
-        Animated.timing(this.state.plane1Fade, { toValue: 1, duration: 1000 }),
-        Animated.timing(this.state.plane2Fade, { toValue: 1, delay: 500, duration: 1000 }),
-        Animated.timing(this.state.plane2YVal, { toValue: 80, delay: 500, duration: 1000 }),
+    const state = this.state
+    Ad.sequence([
+      Ad.timing(state.planeIntro, { toValue: 1, duration: 1000 }),
+      Ad.parallel([
+        Ad.timing(state.plane1Fade, { toValue: 1, duration: 1000 }),
+        Ad.timing(state.plane1YVal, { toValue: -50, duration: 1000 }),
+        Ad.timing(state.plane2Fade, { toValue: 1, delay: 1000, duration: 1000 }),
+        Ad.timing(state.plane2YVal, { toValue: -50, delay: 1000, duration: 1000 }),
+        Ad.timing(state.plane3Fade, { toValue: 1, delay: 1500, duration: 800 }),
+        Ad.timing(state.plane3YVal, { toValue: -20, delay: 1500, duration: 800 }),
+        Ad.timing(state.plane4Fade, { toValue: 1, delay: 1700, duration: 600 }),
+        Ad.timing(state.plane4YVal, { toValue: -20, delay: 1700, duration: 600 }),
+        Ad.timing(state.plane5Fade, { toValue: 1, delay: 1800, duration: 600 }),
+        Ad.timing(state.plane5YVal, { toValue: 100, delay: 1800, duration: 600 }),
+        Ad.timing(state.plane6Fade, { toValue: 1, delay: 1800, duration: 500 }),
+        Ad.timing(state.plane6YVal, { toValue: 60, delay: 1800, duration: 500 }),
+        Ad.timing(state.plane7Fade, { toValue: 1, delay: 1900, duration: 600 }),
+        Ad.timing(state.plane7YVal, { toValue: 60, delay: 1900, duration: 600 }),
+        Ad.timing(state.plane8Fade, { toValue: 1, delay: 1900, duration: 600 }),
+        Ad.timing(state.plane8YVal, { toValue: 160, delay: 1900, duration: 600 }),
+        Ad.timing(state.plane9Fade, { toValue: 1, delay: 2000, duration: 500 }),
+        Ad.timing(state.plane9YVal, { toValue: 160, delay: 2000, duration: 500 }),
+        Ad.timing(state.plane10Fade, { toValue: 1, delay: 2000, duration: 600 }),
+        Ad.timing(state.plane10YVal, { toValue: 150, delay: 2000, duration: 600 }),
       ]),
-      Animated.timing(this.state.planeOutro, { toValue: 1, duration: 1000 }),
+      Ad.timing(state.planeOutro, { toValue: 1, duration: 1000 }),
     ]).start()
   }
 
@@ -69,8 +104,25 @@ export default class IntroSwiperContainer extends React.Component {
             <AirPlanePresenter
               planeIntro={this.state.planeIntro}
               plane1Fade={this.state.plane1Fade}
+              plane1YVal={this.state.plane1YVal}
               plane2Fade={this.state.plane2Fade}
               plane2YVal={this.state.plane2YVal}
+              plane3Fade={this.state.plane3Fade}
+              plane3YVal={this.state.plane3YVal}
+              plane4Fade={this.state.plane4Fade}
+              plane4YVal={this.state.plane4YVal}
+              plane5Fade={this.state.plane5Fade}
+              plane5YVal={this.state.plane5YVal}
+              plane6Fade={this.state.plane6Fade}
+              plane6YVal={this.state.plane6YVal}
+              plane7Fade={this.state.plane7Fade}
+              plane7YVal={this.state.plane7YVal}
+              plane8Fade={this.state.plane8Fade}
+              plane8YVal={this.state.plane8YVal}
+              plane9Fade={this.state.plane9Fade}
+              plane9YVal={this.state.plane9YVal}
+              plane10Fade={this.state.plane10Fade}
+              plane10YVal={this.state.plane10YVal}
               planeOutro={this.state.planeOutro} />
             {this.state.alarmVisible
             ? <SetAlarmContainer verifyStorageKey={this.props.verifyStorageKey} />
--- components/presenters/introPresenters/AirPlanePresenter.js
+++ components/presenters/introPresenters/AirPlanePresenter.js
@@ -3,7 +3,7 @@ import { SafeAreaView, View, Text, Animated } from 'react-native'
 import { Ionicons } from '@expo/vector-icons'
 import ss from '../../StyleSheet'
 
-const AirPlanePresenter = ({ planeIntro, plane1Fade, plane2Fade, plane2YVal, planeOutro }) => (
+const AirPlanePresenter = ({ planeIntro, plane1Fade, plane1YVal, plane2Fade, plane2YVal, plane3Fade, plane3YVal, plane4Fade, plane4YVal, plane5Fade, plane5YVal, plane6Fade, plane6YVal, plane7Fade, plane7YVal, plane8Fade, plane8YVal, plane9Fade, plane9YVal, plane10Fade, plane10YVal, planeOutro }) => (
   <SafeAreaView style={ss.invisiContainer}>
     <View style={[ss.invisiContainer, ss.padding15]}>
       <View style={[ss.flex1, ss.center]}>
@@ -12,10 +12,10 @@ const AirPlanePresenter = ({ planeIntro, plane1Fade, plane2Fade, plane2YVal, pla
         </Animated.View>
       </View>
       <View style={[ss.flex1, ss.center]}>
-        <Animated.View style={{opacity: plane1Fade}}>
+        <Animated.View style={{opacity: plane1Fade, bottom: plane1YVal}}>
           <Ionicons
             name="ios-paper-plane"
-            size={40}
+            size={50}
             style={{color: '#fff'}} />
         </Animated.View>
         <Animated.View style={{opacity: plane2Fade, right: 40, bottom: plane2YVal}}>
@@ -24,8 +24,56 @@ const AirPlanePresenter = ({ planeIntro, plane1Fade, plane2Fade, plane2YVal, pla
             size={40}
             style={{color: '#fff'}} />
         </Animated.View>
+        <Animated.View style={{opacity: plane3Fade, left: 40, bottom: plane3YVal}}>
+          <Ionicons
+            name="ios-paper-plane"
+            size={35}
+            style={{color: '#6a89cc'}} />
+        </Animated.View>
+        <Animated.View style={{opacity: plane4Fade, left: 10, bottom: plane4YVal}}>
+          <Ionicons
+            name="ios-paper-plane"
+            size={35}
+            style={{color: '#82ccdd'}} />
+        </Animated.View>
+        <Animated.View style={{opacity: plane5Fade, right: 50, bottom: plane5YVal}}>
+          <Ionicons
+            name="ios-paper-plane"
+            size={30}
+            style={{color: '#82ccdd'}} />
+        </Animated.View>
+        <Animated.View style={{opacity: plane6Fade, right: 30, bottom: plane6YVal}}>
+          <Ionicons
+            name="ios-paper-plane"
+            size={30}
+            style={{color: '#6a89cc'}} />
+        </Animated.View>
+        <Animated.View style={{opacity: plane7Fade, left: 55, bottom: plane7YVal}}>
+          <Ionicons
+            name="ios-paper-plane"
+            size={45}
+            style={{color: '#fff'}} />
+        </Animated.View>
+        <Animated.View style={{opacity: plane8Fade, left: 10, bottom: plane8YVal}}>
+          <Ionicons
+            name="ios-paper-plane"
+            size={40}
+            style={{color: '#fff'}} />
+        </Animated.View>
+        <Animated.View style={{opacity: plane9Fade, right: 20, bottom: plane9YVal}}>
+          <Ionicons
+            name="ios-paper-plane"
+            size={35}
+            style={{color: '#fff'}} />
+        </Animated.View>
+        <Animated.View style={{opacity: plane10Fade, right: 60, bottom: plane10YVal}}>
+          <Ionicons
+            name="ios-paper-plane"
+            size={30}
+            style={{color: '#fff'}} />
+        </Animated.View>
       </View>
-      <View style={[ss.flex1, ss.center]}>
+      <View style={[ss.flex1, ss.center, ss.addViewSpacing]}>
         <Animated.View style={{opacity: planeOutro}}>
           <Text style={[ss.subHeader, ss.whiteText, ss.centerText]}>Ora will show those requests to other users, and you will receive notifications when they are praying for you.</Text>
         </Animated.View>
