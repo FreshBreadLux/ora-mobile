20bfef4363bd451d8c2b0ad94589be438798ec30
Bobby Lux <robert.michael.lux@gmail.com>
2018-03-05 17:54:28 -0500

    chore: change the titles of reminders set by the intro setAlarmContainer

--- components/containers/introContainers/SetAlarmContainer.js
+++ components/containers/introContainers/SetAlarmContainer.js
@@ -40,7 +40,7 @@ class SetAlarmContainer extends React.Component {
     }
     try {
       const reminderId = await Notifications.scheduleLocalNotificationAsync({
-        title: 'Ora',
+        title: 'Strengthen Your Faith',
         body: 'Time to pray',
         data: { body: 'Time to pray' },
         sound: true
