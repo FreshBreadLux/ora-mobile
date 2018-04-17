33c1703f40e214121a4874a89c679500c01a51b4
Bobby Lux <robert.michael.lux@gmail.com>
2018-04-17 16:10:45 -0400

    feat: add conditional rendering to DonatePresenter

--- components/presenters/profilePresenters/DonatePresenter.js
+++ components/presenters/profilePresenters/DonatePresenter.js
@@ -9,19 +9,27 @@ const DonatePresenter = ({ investmentTotal }) => (
   <SafeAreaView style={ss.whiteContainer}>
     <View style={[ss.whiteContainer, ss.padding15]}>
       <View style={[ss.addMedViewSpacing, ss.topBorder]}>
-        <Text style={ss.body}>Ora is a non-profit app, and it will always be free. This means that we rely on donations from our community to meet all operating expenses, from keeping the servers running to funding future projects. We hope to continue making tools for faith in a technology age that has other priorities, and we're inviting you to be a part of it.{'\n\n'}If you'd like to become a part of our team you can use the link below to sign up as a donor on our website.</Text>
-      </View>
-      <View style={[ss.center, ss.addMedViewSpacing]}>
-        <TouchableOpacity
-          onPress={() => WebBrowser.openBrowserAsync('https://www.oraprayernetwork.com/donor-signup')}>
-          <View style={[ss.row, ss.padding4]}>
-            <Text style={[ss.subBody, ss.darkBlueText, ss.paddingRight7]}>DONOR SIGNUP</Text>
-            <Ionicons
-              name="ios-arrow-forward"
-              size={18}
-              color="#1e3799" />
+        {investmentTotal
+        ? <View>
+            <Text style={ss.body}>Ora is a non-profit app, and it will always be free. This means that we rely on donations from our community to meet all operating expenses, from keeping the servers running to funding future projects. We hope to continue making tools for faith in a technology age that has other priorities, and we're inviting you to be a part of it.{'\n\n'}If you'd like to become a part of our team you can use the link below to sign up as a donor on our website.</Text>
+            <View style={[ss.center, ss.addMedViewSpacing]}>
+              <TouchableOpacity
+                onPress={() => WebBrowser.openBrowserAsync('https://www.oraprayernetwork.com/donor-signup')}>
+                <View style={[ss.row, ss.padding4]}>
+                  <Text style={[ss.subBody, ss.darkBlueText, ss.paddingRight7]}>DONOR SIGNUP</Text>
+                  <Ionicons
+                    name="ios-arrow-forward"
+                    size={18}
+                    color="#1e3799" />
+                </View>
+              </TouchableOpacity>
+            </View>
           </View>
-        </TouchableOpacity>
+        : <View>
+            <Text style={ss.subHeader}>Your Investment Total:</Text>
+            <Text style={[ss.rightText, ss.header]}>{`$${investmentTotal / 100}`}</Text>
+          </View>
+        }
       </View>
     </View>
   </SafeAreaView>
