89eeacf990e072fde718798d0fbf7cacb98f139f
Bobby Lux <robert.michael.lux@gmail.com>
2018-07-03 10:39:32 -0400

    feat: add analytics.js with amplitude functions

---
+++ components/analytics.js
@@ -0,0 +1,30 @@
+import { Amplitude } from 'expo'
+import { AMPLITUDE_TEST_API_KEY } from '../config'
+
+export const ampEvents = {
+  START_REFLECTION: 'START_REFLECTION',
+  LOAD_NEXT_PRAYER: 'LOAD_NEXT_PRAYER',
+  FOLLOW_PRAYER: 'FOLLOW_PRAYER',
+  SEND_LOVE: 'SEND_LOVE'
+}
+
+export const ampInitialize = () => Amplitude.initialize(AMPLITUDE_TEST_API_KEY)
+
+export const ampIdentify = (id, userProperties) => {
+  if (id) {
+    Amplitude.setUserId(id)
+    if (userProperties) {
+      Amplitude.setUserProperties(userProperties)
+    }
+  } else {
+    Amplitude.clearUserProperties()
+  }
+}
+
+export const ampLogEvent = (event, eventProperties) => {
+  if (eventProperties) {
+    Amplitude.logEventWithProperties(event, eventProperties)
+  } else {
+    Amplitude.logEvent(event)
+  }
+}
