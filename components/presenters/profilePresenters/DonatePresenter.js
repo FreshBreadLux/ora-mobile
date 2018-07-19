b7a38754effeacd3b1bb6322443fce5155db3196
Bobby Lux <robert.michael.lux@gmail.com>
2018-07-19 10:22:51 -0500

    chore: add logging to DonatePresenter

--- components/presenters/profilePresenters/DonatePresenter.js
+++ components/presenters/profilePresenters/DonatePresenter.js
@@ -14,7 +14,10 @@ const DonatePresenter = ({ investmentTotal, firstName, lastName }) => (
             <Text style={ss.body}>Ora is a non-profit app, and it will always be free. This means that we rely on donations from our community to meet all operating expenses, from keeping the servers running to funding future projects. We hope to continue making tools for faith in a technology age that has other priorities, and we're inviting you to be a part of it.{'\n\n'}If you'd like to become a part of our team you can use the link below to sign up as a donor on our website.</Text>
             <View style={[ss.center, ss.addLargeViewSpacing]}>
               <TouchableOpacity
-                onPress={() => Linking.openURL('https://www.oraprayernetwork.com/donor-signup')}>
+                onPress={() => {
+                  console.log('firing Linking.openURL:', Linking.openURL)
+                  Linking.openURL('https://www.oraprayernetwork.com/donor-signup')
+                }}>
                 <View style={[ss.row, ss.padding4]}>
                   <Text style={[ss.subBody, ss.darkBlueText, ss.paddingRight7]}>DONOR SIGNUP</Text>
                   <Ionicons
