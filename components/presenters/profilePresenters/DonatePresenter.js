1952f9be2ebc4174c71746e5570eb29e41d07dbc
Bobby Lux <robert.michael.lux@gmail.com>
2018-07-19 10:05:50 -0500

    feat: change openWebBrowserAsyc to openURL in DonatePresenter

--- components/presenters/profilePresenters/DonatePresenter.js
+++ components/presenters/profilePresenters/DonatePresenter.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Text, View, SafeAreaView, TouchableOpacity } from 'react-native'
+import { Text, View, SafeAreaView, TouchableOpacity, Linking } from 'react-native'
 import { WebBrowser } from 'expo'
 import { Ionicons } from '@expo/vector-icons'
 import ss from '../../StyleSheet'
@@ -14,7 +14,7 @@ const DonatePresenter = ({ investmentTotal, firstName, lastName }) => (
             <Text style={ss.body}>Ora is a non-profit app, and it will always be free. This means that we rely on donations from our community to meet all operating expenses, from keeping the servers running to funding future projects. We hope to continue making tools for faith in a technology age that has other priorities, and we're inviting you to be a part of it.{'\n\n'}If you'd like to become a part of our team you can use the link below to sign up as a donor on our website.</Text>
             <View style={[ss.center, ss.addLargeViewSpacing]}>
               <TouchableOpacity
-                onPress={() => WebBrowser.openBrowserAsync('https://www.oraprayernetwork.com/donor-signup')}>
+                onPress={() => Linking.openURL('https://www.oraprayernetwork.com/donor-signup')}>
                 <View style={[ss.row, ss.padding4]}>
                   <Text style={[ss.subBody, ss.darkBlueText, ss.paddingRight7]}>DONOR SIGNUP</Text>
                   <Ionicons
