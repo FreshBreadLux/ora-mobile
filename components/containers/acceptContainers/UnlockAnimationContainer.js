a418e409f079f95e6c41df6a514104379b1e6578
Bobby Lux <robert.michael.lux@gmail.com>
2018-08-12 18:33:16 -0400

    feat: assemble the lock in the UnlockAnimationContainer

--- components/containers/acceptContainers/UnlockAnimationContainer.js
+++ components/containers/acceptContainers/UnlockAnimationContainer.js
@@ -35,6 +35,62 @@ class KeyContainer extends React.Component {
             top: 42.8672427,
           }}
           source={require('../../../assets/images/Key/key-ring.png')} />
+        <Animated.Image
+          style={{
+            position: 'absolute',
+            height: 25.9230165,
+            width: 21.2097408,
+            left: 23.7902592,
+            top: 64.0769835,
+          }}
+          source={require('../../../assets/images/Key/key-head.png')} />
+        <Animated.Image
+          style={{
+            position: 'absolute',
+            height: 25.9230165,
+            width: 21.2097408,
+            left: 44.9999937,
+            top: 64.0769835,
+          }}
+          source={require('../../../assets/images/Key/key-head.png')} />
+        <Animated.Image
+          style={{
+            position: 'absolute',
+            height: 4.71327572,
+            width: 42.4194815,
+            left: 23.7902592,
+            top: 59.3637078,
+          }}
+          source={require('../../../assets/images/Key/key-mid.png')} />
+        <Animated.Image
+          style={{
+            position: 'absolute',
+            height: 4.71327572,
+            width: 42.4194815,
+            left: 23.7902592,
+            top: 59.3637078,
+          }}
+          source={require('../../../assets/images/Key/key-mid.png')} />
+        <Animated.Image
+          style={{
+            position: 'absolute',
+            height: 9.42655144,
+            width: 4.71327572,
+            left: 26.1468971,
+            top: 59.3637078,
+            transform: [{rotate: '270deg'}]
+          }}
+          source={require('../../../assets/images/Key/key-end.png')} />
+        <Animated.Image
+          style={{
+            position: 'absolute',
+            height: 9.42655144,
+            width: 4.71327572,
+            left: 59.139827,
+            top: 59.3637078,
+            transform: [{rotate: '90deg'}]
+          }}
+          source={require('../../../assets/images/Key/key-end.png')} />
       </View>
     )
   }
