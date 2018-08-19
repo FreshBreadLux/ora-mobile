c03fabdf06529750dcbff637dea36075871d864a
Bobby Lux <robert.michael.lux@gmail.com>
2018-08-19 15:29:50 -0400

    feat: remove SavedRewardSinglePresenter from MainNav

--- components/MainNav.js
+++ components/MainNav.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Platform } from 'react-native'
 import { StackNavigator, TabNavigator, TabBarBottom } from 'react-navigation'
 import { AcceptContainer, PrayerContainer, FollowContainer, SubmitContainer, ProfileContainer, AlarmContainer, ShareOraContainer, RegisterOraMissionaryContainer, RewardContainer, HomeContainer, SurveySwiperContainer } from './containers'
-import { FollowScrollPresenter, PrayerScrollPresenter, AboutPresenter, ChoirRankPresenter, DonatePresenter, PrayerHeaderPresenter, TraditionalPrayersPresenter, ReflectionFullTextPresenter, RewardFullTextPresenter, SavedRewardsListPresenter, SavedRewardSinglePresenter } from './presenters'
+import { FollowScrollPresenter, PrayerScrollPresenter, AboutPresenter, ChoirRankPresenter, DonatePresenter, PrayerHeaderPresenter, TraditionalPrayersPresenter, ReflectionFullTextPresenter, RewardFullTextPresenter, SavedRewardsListPresenter } from './presenters'
 import { Constants } from 'expo'
 import { Ionicons } from '@expo/vector-icons'
 
@@ -242,12 +242,6 @@ const CardStackNav = StackNavigator({
         headerStyle,
       }
     },
-  },
-  SavedRewardSingle: {
-    screen: SavedRewardSinglePresenter,
-    navigationOptions: {
-      header: null,
-    }
   },
   SurveySwiper: {
     screen: SurveySwiperContainer,
