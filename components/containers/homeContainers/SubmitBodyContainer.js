9e29f039b6415e7e74d74413fae8b08b573cafd3
Bobby Lux <robert.michael.lux@gmail.com>
2018-09-11 14:41:31 -0400

    feat: set up responsive height textInput in SubmitBodyContainer and Presenter

--- components/containers/homeContainers/SubmitBodyContainer.js
+++ components/containers/homeContainers/SubmitBodyContainer.js
@@ -1,18 +1,89 @@
 import React from 'react'
+import { Keyboard, Animated } from 'react-native'
 import { SubmitBodyPresenter } from '../../presenters'
 
 class SubmitBodyContainer extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      body: null
+      startHeight: null,
+      animatedHeight: null,
     }
+    this.keyboardWillShow = this.keyboardWillShow.bind(this)
+    this.keyboardDidShow = this.keyboardDidShow.bind(this)
+    this.keyboardWillHide = this.keyboardWillHide.bind(this)
+    this.keyboardDidHide = this.keyboardDidHide.bind(this)
+    this.handleOnLayout = this.handleOnLayout.bind(this)
+    this.focusBodyTextInput = this.focusBodyTextInput.bind(this)
+    this.referenceBodyTextInput = this.referenceBodyTextInput.bind(this)
+  }
+
+  componentWillMount () {
+    this.keyboardWillShowSub = Keyboard.addListener('keyboardWillShow', this.keyboardWillShow)
+    this.keyboardDidShowSub = Keyboard.addListener('keyboardDidShow', this.keyboardDidShow)
+    this.keyboardWillHideSub = Keyboard.addListener('keyboardWillHide', this.keyboardWillHide)
+    this.keyboardDidHideSub = Keyboard.addListener('keyboardDidHide', this.keyboardDidHide)
+  }
+
+  componentWillUnmount() {
+    this.keyboardWillShowSub.remove()
+    this.keyboardDidShowSub.remove()
+    this.keyboardWillHideSub.remove()
+    this.keyboardDidHideSub.remove()
+  }
+
+  keyboardWillShow(event) {
+    Animated.timing(this.state.animatedHeight, {
+      duration: event.duration,
+      toValue: this.state.startHeight - event.endCoordinates.height,
+    }).start()
+  }
+
+  keyboardDidShow(event) {
+    Animated.timing(this.state.animatedHeight, {
+      duration: 100,
+      toValue: this.state.startHeight - event.endCoordinates.height,
+    }).start()
+  }
+
+  keyboardWillHide(event) {
+    Animated.timing(this.state.animatedHeight, {
+      duration: event.duration,
+      toValue: 0,
+    }).start()
+  }
+
+  keyboardDidHide(event) {
+    Animated.timing(this.state.animatedHeight, {
+      duration: event.duration,
+      toValue: 0,
+    }).start()
+  }
+
+  handleOnLayout(event) {
+    let startHeight = event.nativeEvent.layout.height - 50
+    this.setState({
+      startHeight: startHeight,
+      animatedHeight: new Animated.Value(startHeight)},
+      this.focusBodyTextInput
+    )
+  }
+
+  referenceBodyTextInput(ref) {
+    this.bodyTextInput = ref
+  }
+
+  focusBodyTextInput() {
+    this.bodyTextInput.focus()
   }
 
   render() {
-    console.log('navigation:', this.props.navigation)
     return (
-      <SubmitBodyPresenter navigation={this.props.navigation} />
+      <SubmitBodyPresenter
+        navigation={this.props.navigation}
+        handleOnLayout={this.handleOnLayout}
+        animatedHeight={this.state.animatedHeight}
+        referenceBodyTextInput={this.referenceBodyTextInput} />
     )
   }
 }
--- components/presenters/homePresenters/SubmitBodyPresenter.js
+++ components/presenters/homePresenters/SubmitBodyPresenter.js
@@ -1,11 +1,37 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { connect } from 'react-redux'
+import { View, Text, TextInput, Animated } from 'react-native'
 import ss from '../../StyleSheet'
+import { setAnonymousBody } from '../../../store';
 
-const SubmitBodyPresenter = ({}) => (
-  <View style={[ss.whiteContainer, ss.center]}>
-    <Text>This is the body submission presenter</Text>
+const SubmitBodyPresenter = ({ handleOnLayout, animatedHeight, referenceBodyTextInput, dispatchSetAnonymousBody }) => (
+  <View
+    onLayout={handleOnLayout}
+    style={ss.invisiContainer}>
+    <Text style={[ss.subBody, ss.padding10]}>Describe your prayer request here</Text>
+    <Animated.View
+      style={{height: animatedHeight ? animatedHeight : 600 }}>
+      <TextInput
+        style={[ss.fullWidth, ss.fullHeight, ss.body, ss.padding10, {
+          borderTopWidth: 1,
+          borderTopColor: '#ccc',
+          backgroundColor: '#fff',
+        }]}
+        ref={referenceBodyTextInput}
+        onChangeText={dispatchSetAnonymousBody}
+        underlineColorAndroid="transparent"
+        placeholder="Write your prayer requests so that your readers will have a contemplative and fruitful prayer experience. There is no character limit."
+        placeholderTextColor="#999"
+        multiline={true}
+        selectionColor="rgb(69, 119, 238)"
+        keyboardType="default" />
+    </Animated.View>
+    <View style={ss.whiteContainer} />
   </View>
 )
 
-export default SubmitBodyPresenter
+const mapDispatch = dispatch => ({
+  dispatchSetAnonymousBody: body => dispatch(setAnonymousBody(body))
+})
+
+export default connect(null, mapDispatch)(SubmitBodyPresenter)
